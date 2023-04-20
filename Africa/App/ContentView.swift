942f935250b4fab0e17259b26bd9e33f846021e9
studioOwol <from.owol@gmail.com>
2023-04-20 15:18:21 +0900

    Video Player - Part 1

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top:0, leading: 0, bottom: 0, trailing: 0))
+                    
                 
                 ForEach(animals) { animal in
                     NavigationLink(destination: AnimalDetailView(animal: animal)){
--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct VideoListView: View {
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                }//: Loop
+            }//: List
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(action: {
+                        //Shuffle videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }){
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        }//: Navigation
     }
 }
 
