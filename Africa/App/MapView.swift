0f701a791a619899968bad0cc35bae1fc3be6907
studioOwol <from.owol@gmail.com>
2023-04-20 21:27:58 +0900

    Complex Map with custom Annotations - Part 2

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -31,13 +31,62 @@ struct MapView: View {
             //MapMarker(coordinate: item.location, tint: .accentColor)
             
             // (B) Custom Basic Annotation (it could be interactive)
-            MapAnnotation(coordinate: item.location) {
-                Image("logo")
+//            MapAnnotation(coordinate: item.location) {
+//                Image("logo")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 32, height: 32, alignment: .center)
+//            }//: Annotation
+            
+            // (C) Custom Advanced Annotation (it could be interactive)
+            MapAnnotation(coordinate: item.location){
+                MapAnnotationView(location: item)
+            }
+        }) //: Map
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 32, height: 32, alignment: .center)
-            }//: Annotation
-        })
+                .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+            }//: HStack
+                .padding(.vertical, 12)
+                .padding(.horizontal,16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding()
+            ,   alignment: .top
+        )
+        
     }
 }
 
