917d3a4ac5a5eac58d7b481b633cd9f5ff00baa5
studioOwol <from.owol@gmail.com>
2023-04-20 16:41:56 +0900

    Complex Map with custom Annotations - Part 1

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     var body: some View {
-        Text("Map")
+        //MARK: - No1 Basic Map
+        //Map(coordinateRegion: $region)
+        
+        //MARK: - No2 Advanced Map
+        Map(coordinateRegion: $region, annotationItems: locations,
+            annotationContent: { item in
+            // (A) Marker: New Style (always static)
+            //MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // (B) Custom Basic Annotation (it could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }//: Annotation
+        })
     }
 }
+
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Nahyun on 2023/04/20.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
